e17a3de486ed33782e3016b3d7d2fd0f82be79c3
Hsuan Lee <hsuangm@gmail.com>
2023-06-22 22:34:34 +0800

    chore: init integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import solidJs from "@astrojs/solid-js";
+import mdx from "@astrojs/mdx";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), solidJs(), mdx()],
+  output: "server",
+  adapter: vercel()
+});
